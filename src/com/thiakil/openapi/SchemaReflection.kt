9553f9d165b3dc2229d150b83a14c24fff7a9e9c
Thiakil <xander@thiakil.com>
2020-01-02 01:03:38 +0800

    fix array annotation additions

--- src/com/thiakil/openapi/SchemaReflection.kt
+++ src/com/thiakil/openapi/SchemaReflection.kt
@@ -1,10 +1,16 @@
 package com.thiakil.openapi
 
+import com.fasterxml.jackson.annotation.JsonView
 import io.swagger.v3.core.util.AnnotationsUtils
+import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.media.*
+import org.apache.commons.lang3.StringUtils
 import java.math.BigInteger
 import java.util.*
-import kotlin.reflect.*
+import kotlin.reflect.KClass
+import kotlin.reflect.KClassifier
+import kotlin.reflect.KProperty
+import kotlin.reflect.KType
 import kotlin.reflect.full.findAnnotation
 import kotlin.reflect.full.memberProperties
 import kotlin.reflect.jvm.javaField
@@ -24,18 +30,18 @@ class LazyWithReceiver<This,Return>(val initializer:This.()->Return)
 
 val KClassifier.openApiSchema: Schema<*>
     get() = when (this){
+        String::class -> StringSchema()
+        Int::class, Long::class -> IntegerSchema()
+        Float::class, Double::class, BigInteger::class -> NumberSchema()
+        Boolean::class -> BooleanSchema()
+        Date::class -> DateTimeSchema()
         is KClass<*> -> this.openApiSchema
         else -> throw RuntimeException("Classifier isn't KClass??")
     }
 
 val KType.openApiSchema : Schema<*> by LazyWithReceiver<KType, Schema<*>> {
     when (val classifier = classifier) {
-        String::class -> StringSchema()
-        Int::class, Long::class -> IntegerSchema()
-        Float::class, Double::class, BigInteger::class -> NumberSchema()
-        Boolean::class -> BooleanSchema()
         List::class -> arrayType()
-        Date::class -> DateTimeSchema()
         null -> throw RuntimeException("Classifier was null??")
         else -> {
             if (classifier.javaClass.isArray) {
@@ -54,6 +60,62 @@ private fun KType.arrayType(): ArraySchema {
     }
 }
 
+//copied so we can use ArraySchema.arraySchema
+fun getArraySchema(arraySchema: io.swagger.v3.oas.annotations.media.ArraySchema?): ArraySchema? {
+    if (arraySchema == null || !AnnotationsUtils.hasArrayAnnotation(arraySchema)) {
+        return null
+    }
+    val arraySchemaObject = ArraySchema()
+    if (arraySchema.uniqueItems) {
+        arraySchemaObject.uniqueItems = arraySchema.uniqueItems
+    }
+    if (arraySchema.maxItems > 0) {
+        arraySchemaObject.maxItems = arraySchema.maxItems
+    }
+    if (arraySchema.minItems < Int.MAX_VALUE) {
+        arraySchemaObject.minItems = arraySchema.minItems
+    }
+    if (arraySchema.extensions.isNotEmpty()) {
+        val extensions = AnnotationsUtils.getExtensions(*arraySchema.extensions)
+        if (extensions != null) {
+            for (ext in extensions.keys) {
+                arraySchemaObject.addExtension(ext, extensions[ext])
+            }
+        }
+    }
+    if (arraySchema.schema.implementation == Void::class.java) {
+        AnnotationsUtils.getSchemaFromAnnotation(arraySchema.schema, null, null)
+            .ifPresent { schema: Schema<*> ->
+                if (StringUtils.isNotBlank(schema.type) || StringUtils.isNotBlank(schema.`$ref`)) {
+                    arraySchemaObject.items = schema
+                }
+            }
+    }
+    AnnotationsUtils.getSchemaFromAnnotation(arraySchema.arraySchema, null, null).ifPresent { schema ->
+        arraySchemaObject.apply {
+            title = schema.title
+            uniqueItems = schema.uniqueItems
+            required = schema.required
+            description = schema.description
+            nullable = schema.nullable
+            readOnly = schema.readOnly
+            writeOnly = schema.writeOnly
+            example = schema.example
+            externalDocs = schema.externalDocs
+            deprecated = schema.deprecated
+            xml = schema.xml
+            extensions = schema.extensions
+        }
+    }
+    return arraySchemaObject
+}
+
+fun getAnnotatedSchema(prop: KProperty<*>): Schema<*>? {
+    return AnnotationsUtils.getSchemaFromAnnotation(prop.findAnnotationMulti(), null).orElseGet {
+        getArraySchema(prop.findAnnotationMulti())
+    }
+}
+
 val <T : Any> KClass<T>.openApiSchema: Schema<*> by LazyWithReceiver<KClass<T>, Schema<*>> {
     when {
         this === List::class -> ArraySchema().also {
@@ -68,18 +130,28 @@ val <T : Any> KClass<T>.openApiSchema: Schema<*> by LazyWithReceiver<KClass<T>, 
             schema.type = "object"
 
             this.memberProperties.forEach { prop ->
-                val type: Schema<*> = AnnotationsUtils.getSchemaFromAnnotation(prop.findAnnotationMulti(), null).map {
-                    if (it.type.isBlank || it.name.isBlank) {
-                        val reflectedSchema = prop.returnType.openApiSchema
-                        if (it.type.isBlank) {
-                            it.type = reflectedSchema.type
-                        }
-                        if (it.name.isBlank) {
+                val reflectedSchema = prop.returnType.openApiSchema
+                val annotatedSchema = getAnnotatedSchema(prop)
+                val type: Schema<*> = when {
+                    annotatedSchema == null -> reflectedSchema
+                    reflectedSchema is ArraySchema && annotatedSchema is ArraySchema -> {
+                        annotatedSchema.also {
                             it.name = reflectedSchema.name
+                            if (it.items != null)
+                                it.items.type = reflectedSchema.items.type //force type to be from reflection
+                            else
+                                it.items = reflectedSchema.items
                         }
                     }
-                    return@map it
-                }.orElseGet { prop.returnType.openApiSchema }
+                    reflectedSchema !is ArraySchema && annotatedSchema !is ArraySchema -> {
+                        annotatedSchema.also {
+                            it.name = reflectedSchema.name
+                            it.type = reflectedSchema.type //force type to be from reflection
+                        }
+                    }
+                    else ->
+                        throw IllegalStateException("Reflected schema & annotation schema not same types: ${this.simpleName}")
+                }
                 schema.addProperties(prop.name, type)
             }
         }
