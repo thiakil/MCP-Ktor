84d217cbf45b49eef1394d301a30e36204f3581c
Thiakil <xander@thiakil.com>
2020-01-26 17:21:37 +0800

    get member endpoints

--- src/com/thiakil/mcp/endpoints/GetMemberEndpoint.kt
+++ src/com/thiakil/mcp/endpoints/GetMemberEndpoint.kt
@@ -1,9 +1,18 @@
 package com.thiakil.mcp.endpoints
 
+import com.thiakil.mcp.McpState
+import com.thiakil.mcp.endpoints.ErrorCode.NOT_FOUND
+import cuchaz.enigma.translation.representation.entry.ClassDefEntry
+import cuchaz.enigma.translation.representation.entry.ClassEntry
+import cuchaz.enigma.translation.representation.entry.MethodEntry
+import io.ktor.application.ApplicationCall
 import io.ktor.http.HttpMethod
+import io.ktor.util.pipeline.PipelineContext
 import io.swagger.v3.oas.models.parameters.Parameter
 import java.util.*
 import kotlin.reflect.KClass
+
+private val ONLY_NUMBER_REGEX = Regex("^\\d+$")
 
 open class GetMemberEndpoint<T : MemberInfo>(
     responseType: KClass<T>,
@@ -28,29 +37,28 @@ open class GetMemberEndpoint<T : MemberInfo>(
 )
 
 @Suppress("unused")
-open class MemberInfo {
-    var mc_version_code: String = ""
-    var obf_name: String = ""
-    var mcp_name: String? = null
-    var srg_name: String = ""
-    var class_pkg_name: String = ""
-    var class_srg_name: String = ""
-    var class_obf_name: String = ""
-    var locked: Boolean = false
-    var obf_member_base_class: String? = null
-    var srg_member_base_class: String? = null
-    var obf_descriptor: String = ""
-    var srg_descriptor: String? = null
-    var is_public: Boolean = true
-    var comment: String? = null
-    var last_modified: MemberLastUpdate? = null
-}
+open class MemberInfo(//var mcVersionCode: String = ""
+    //val obfName: String = ""
+    val mcpName: String?,
+    val srgName: String,
+    //val classPkgName: String = ""
+    val className: String,
+    //val classObfName: String = ""
+    //val locked: Boolean = false
+    //val obfDescriptor: String = ""
+    val descriptor: String,
+    val visibility: String,
+    val comment: String?,
+    val lastModified: MemberLastUpdate?
+)
 
 @Suppress("unused")
-class MethodMemberInfo: MemberInfo() {
-    var is_constrctor: Boolean = false
-    var params: List<ParamLite>? = null
-}
+class MethodMemberInfo(
+    mcpName: String?, srgName: String, className: String, descriptor: String, visibility: String, comment: String?, lastModified: MemberLastUpdate?,
+    val params: List<ParamLite>?,
+    //val obfMemberBaseClass: String?,
+    val classesFoundIn: List<String>
+) : MemberInfo(mcpName, srgName, className, descriptor, visibility, comment, lastModified)
 
 data class ParamLite(val srg_name: String, val mcp_name: String?)
 
@@ -63,8 +71,31 @@ object GetField : GetMemberEndpoint<MemberInfo>(
     apiPath = "/fields/{name}",
     pythonCallback = "getMember",
     description = "Returns field information. Defaults to current version. Version can be for MCP or MC.",
-    parameters = listOf(parameter("class", required = false), Endpoints.NAME_PATH_ARG, Endpoints.OPTIONAL_VERSION)
-)
+    parameters = listOf(/*parameter("class", required = false),*/ Endpoints.NAME_PATH_ARG/*, Endpoints.OPTIONAL_VERSION*/)
+){
+    override fun PipelineContext<Unit, ApplicationCall>.handleEndpoint(appData: McpState): MemberInfo {
+        val name = context.parameters["name"] ?: throw ErrorResponseException(ErrorCode.BAD_PARAMETERS, "Missing name parameter")
+        val filter: (String) -> Boolean = if (ONLY_NUMBER_REGEX.matches(name)) {
+            val matchStr = "field_${name}_"
+            { it.startsWith(matchStr) }
+        } else {
+            { it == name }
+        }
+        return appData.index.fields.keys.find(filter)?.let {
+            val field = appData.index.fields[it]!!
+            return@let MemberInfo(
+                srgName = field.name,
+                className = field.parent!!.fullName,
+                descriptor = field.desc.toString(),
+                visibility =field.access.visibilityString(),
+                //todo
+                mcpName = null,
+                comment = null,
+                lastModified = null
+            )
+        } ?: throw ErrorResponseException(NOT_FOUND, "Field not found")
+    }
+}
 
 @Endpoint
 object GetMethod : GetMemberEndpoint<MethodMemberInfo>(
@@ -74,4 +105,43 @@ object GetMethod : GetMemberEndpoint<MethodMemberInfo>(
     pythonCallback = "getMember",
     description = "Returns method information. Defaults to current version. Version can be for MCP or MC.",
     parameters = listOf(parameter("class", required = false), Endpoints.NAME_PATH_ARG, Endpoints.OPTIONAL_VERSION)
-)
+) {
+    override fun PipelineContext<Unit, ApplicationCall>.handleEndpoint(appData: McpState): MethodMemberInfo {
+        val name = context.parameters["name"] ?: throw ErrorResponseException(ErrorCode.BAD_PARAMETERS, "Missing name parameter")
+        val match = if (ONLY_NUMBER_REGEX.matches(name)) {
+            appData.index.methodsByIndex.get(name.toInt())
+        } else {
+            appData.index.methods.get(name)
+        }
+        return match?.let { methods ->
+            val classesFoundIn = methods.map { it.parent!! }.sortedBy { it.fullName }
+            val baseMethod = if (methods.size == 1) {
+                methods.first()
+            } else {
+                methods.find { mth->
+                    var cls: ClassDefEntry? = (mth.parent as ClassDefEntry).superClass?.let { appData.index.classes[it] }
+                    while(cls != null) {
+                        if (classesFoundIn.contains(cls)){
+                            return@find false
+                        }
+                        cls = cls.superClass?.let { appData.index.classes[it] }
+                    }
+                    return@find true
+                } ?: error("didn't find a base method")
+                //methods.first()
+            }
+            return@let MethodMemberInfo(
+                srgName = baseMethod.name,
+                className = baseMethod.parent!!.fullName,
+                descriptor = baseMethod.desc.toString(),
+                visibility = baseMethod.access.visibilityString(),
+                //todo
+                mcpName = null,
+                comment = null,
+                lastModified = null,
+                classesFoundIn = classesFoundIn.map(ClassEntry::getFullName),
+                params = appData.index.methodParams.get(baseMethod).map { ParamLite(it, null) }
+            )
+        } ?: throw ErrorResponseException(NOT_FOUND, "Method not found")
+    }
+}
